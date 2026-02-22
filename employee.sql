83c239f297e34baad14837b9d6bfacd842660022
ghodekarom <omghodekar999@gmail.com>
2026-02-22 16:27:35 +0530

    database schema

---
+++ employee.sql
@@ -0,0 +1,12 @@
+drop database if exists employeedb;
+create database employeedb;
+\c employeedb
+
+drop table if exists employee;
+create table employee(id serial primary key,name varchar(20),email varchar(20),department varchar(20),designation varchar(20),salary float);
+
+insert into employee(name,email,department,designation,salary) values('Om Ghodekar','om@gmail.com','it','fullstack developer',28000);
+insert into employee(name,email,department,designation,salary) values('Rudresh Honrao','rudresh@gmail.com','it','backend developer',25000);
+insert into employee(name,email,department,designation,salary) values('Vinayak Ghule','vinayak@gmai.com','it','frontend developer',22000);
+
+select * from employee;
